e1194d007eea2287424359ec57c552a5f50e4a2b
SiHoon61 <dlatlgns000@gmail.com>
2023-10-07 01:45:18 +0900

    test_branch 테스트커밋1

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>hi</p>
-        <p>please..</p>
-        <p>hello</p>
-        <p>hey</p>
+        <p>'야성아'가 사라졌다............</p>
         <a
           className="App-link"
           href="https://reactjs.org"
