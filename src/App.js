a6987af58a04b413cb043f71ccb1970f403544bb
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-07 03:13:00 +0900

    App.js 코드 초기화

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>hi</p>
-        <p>please..</p>
-        <p>hello</p>
-        <p>hey</p>
-        <p>살려줘..</p>
-        <p>'야성아'가 사라졌다............</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
