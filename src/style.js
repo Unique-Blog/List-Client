991b86cde4f4fb1c4be73d3282ba9d368ba2e99d
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-07 11:39:08 +0900

    modify: 배경이미지 조정

--- src/style.js
+++ src/style.js
@@ -1,10 +1,13 @@
 import styled from 'styled-components';
 import bg from './images/main_bg.jpg';
 export const GlobalBackground = styled.div`
+    position: fixed;
+    top: 5%;
+    left: 5%;   
     background-image: url(${bg});
     background-repeat: no-repeat;
-    background-size: cover; /* 'cover'로 수정 */
+    background-size: contain; /* 'cover'로 수정 */
     background-position: center;
-    width: 100vw;
-    height: 100vh;
+    width: 90vw;
+    height: 90vh;
 `;
