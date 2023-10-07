eb7ea7deb0e4bacbbfb090dce1fbf77261c6bc91
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-07 15:38:01 +0900

    Modify: 로그인 페이지 수정 - 로그인 페이지에 Form component 대신에 직접 코드 작성 - 서버주소 입력 후 400에러

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,73 +3,43 @@ import { Form0, Input0 } from './style';
 import axios from 'axios'; // axios 라이브러리를 가져옵니다.
 
 const LoginForm = () => {
-  const [formData, setFormData] = useState({
-    id: '',
-    password: '',
-  });
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleTogglePassword = () => {
-    setShowPassword(!showPassword);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // axios를 사용하여 서버로 POST 요청을 보냅니다.
-    axios
-      .post('서버의 주소', formData, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-      .then((response) => {
-        const data = response.data;
-        if (data.message === 'SUCCESS') {
-          alert('회원가입 성공!');
-        } else {
-          alert('회원가입 실패...');
-        }
-      })
-      .catch((error) => {
-        console.error('에러 발생:', error);
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('/api/login', {
+        username: username,
+        password: password
       });
+
+      // 서버에서 반환한 데이터를 기반으로 로그인 상태를 처리할 수 있습니다.
+      console.log('로그인 성공:', response.data);
+    } catch (error) {
+      // 로그인 실패 시 처리
+      console.error('로그인 실패:', error);
+    }
   };
+
 
   return (
     <>
-      <Form0 onSubmit={handleSubmit}>
-        <Input0
-          type="text"
-          name="id"
-          value={formData.id}
-          onChange={handleChange}
-          placeholder="아이디"
-        />
-        <div className="password-toggle">
-          <Input0
-            type={showPassword ? 'text' : 'password'}
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder="비밀번호"
-          />
-          <label className="checkbox-label">
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={handleTogglePassword}
-            />
-            Show Password
-          </label>
-        </div>
-      </Form0>
+      <div>
+      <input
+        type="text"
+        placeholder="사용자 이름"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>로그인</button>
+    </div>
+
     </>
   );
 };
