99be6799f2b64e7334f9a11b53cb7cb3b4f2ceed
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-07 09:53:35 +0900

    배경 이미지

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+//부트스트랩 라이브러리 import
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { GlobalBackground } from './style';
+//pages
 
 function App() {
 
   return (
     <div className="App">
-
+      <GlobalBackground>
+      <div>hihihi</div>
+      <div>hihihi</div>
+      <div>hihihi</div>
+      <div>hihihi</div>
+      </GlobalBackground>
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import bg from './images/main_bg.png';
+
+export const GlobalBackground = styled.div`
+    background-image: url(${bg});
+    background-repeat: no-repeat;
+    background-size: cover; /* 'cover'로 수정 */
+    background-position: center;
+    width: 100vw;
+    height: 100vh;
+`;
