6518f69384e02cde0047fc993dab5708f000edae
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-10-07 10:06:06 +0900

    Modify: 이름 변경 -pngdptj jpg로 변경

--- src/style.js
+++ src/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import bg from './images/main_bg.png';
+import bg from './images/main_bg.jpg';
 
 export const GlobalBackground = styled.div`
     background-image: url(${bg});
